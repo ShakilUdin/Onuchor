7990c9ad07c09e305c184917ad96ad39fbfa98a5
Shakil Uddin <shakil.uddin@yahoo.com>
2022-02-08 19:36:04 +0600

    All Models and Route Added

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -7,8 +7,35 @@ router.get(`/`, async (req, res) =>{
 
     if(!categoryList) {
         res.status(500).json({success: false})
-    } 
+    }
     res.send(categoryList);
 })
 
-module.exports =router;
+router.post(`/`, async (req, res)=>{
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color:req.body.color,
+    })
+    category = await category.save();
+
+    if(!category){
+        return res.status(404).send('The Category cannot be created');
+    }
+    res.send(category);
+})
+
+router.delete(`/:id`, (req, res)=>{
+    Category.findByIdAndRemove(req.params.id).then(category =>{
+        if(category){
+            return res.status(200).json({success: true, message: 'Thr Category Is Deleted'});
+        }
+        else {
+            return res.status(404).json({success: false, message: 'Category Not Found'});
+        }
+    })
+        .catch(err=>{
+            return res.send(400).json({success: false, error: err});
+        })
+})
+module.exports = router;
